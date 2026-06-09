493fb9d001fbcd1ee3f07000db3e259d51a58edc
EduardGuilherme <eduguilherme48@gmail.com>
2026-06-09 20:19:17 -0300

    add get routes

--- JavaAws/src/main/java/br/com/java/aws/JavaAws/JavaAwsApplication.java
+++ JavaAws/src/main/java/br/com/java/aws/JavaAws/JavaAwsApplication.java
@@ -2,12 +2,19 @@ package br.com.java.aws.JavaAws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class JavaAwsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaAwsApplication.class, args);
+		System.out.println("ok");
+	}
+	@GetMapping
+	public String response(){
+		System.out.println("ok");
+		return "ok";
 	}
 
 }
