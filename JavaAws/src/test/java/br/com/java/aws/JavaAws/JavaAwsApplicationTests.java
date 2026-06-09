49d4cefc56b7c7f92042716a886fcf030c6daf3f
EduardGuilherme <eduguilherme48@gmail.com>
2026-06-09 20:02:08 -0300

    first commit

---
+++ JavaAws/src/test/java/br/com/java/aws/JavaAws/JavaAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java.aws.JavaAws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
